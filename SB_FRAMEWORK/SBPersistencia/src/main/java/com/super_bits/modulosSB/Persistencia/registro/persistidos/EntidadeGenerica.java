060627d4dab0e6cd969ab7869bb986043ad4d8a5
root <root@salvio.marketingparaweb.com.br>
2016-04-04 10:40:29 -0300

    Atualizacao automática

--- SB_FRAMEWORK/SBPersistencia/src/main/java/com/super_bits/modulosSB/Persistencia/registro/persistidos/EntidadeGenerica.java
+++ SB_FRAMEWORK/SBPersistencia/src/main/java/com/super_bits/modulosSB/Persistencia/registro/persistidos/EntidadeGenerica.java
@@ -160,34 +160,5 @@ public abstract class EntidadeGenerica extends ItemGenerico implements Serializa
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    @Override
-    public String getNomeCurto() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public int getId() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String getNomeDoObjeto() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public String getNome() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setId(int pID) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void setNome(String pNome) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
 
 }
