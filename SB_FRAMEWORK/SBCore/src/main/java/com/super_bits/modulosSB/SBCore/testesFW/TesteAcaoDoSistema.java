88c4cde5f356ccb1e164cfcda5769ce0fdd75467
Desenvolvedor <desenvolvedor@localhost.localdomain>
2016-04-14 14:09:56 -0300

    Atualizacao automática

--- SB_FRAMEWORK/SBCore/src/main/java/com/super_bits/modulosSB/SBCore/testesFW/TesteAcaoDoSistema.java
+++ SB_FRAMEWORK/SBCore/src/main/java/com/super_bits/modulosSB/SBCore/testesFW/TesteAcaoDoSistema.java
@@ -25,121 +25,39 @@ public abstract class TesteAcaoDoSistema extends TesteJunit {
     private boolean validarAcoesNaoConfiguradas = false;
 
     private boolean validatAcao(ItfAcaoDoSistema pAcaoDoSistema) {
+        try {
+            if (pAcaoDoSistema.getEnumAcaoDoSistema() == null) {
+                throw new UnsupportedOperationException("O enum da ação do sistema" + pAcaoDoSistema + " não foi definido");
+            }
 
-        if (pAcaoDoSistema.getEnumAcaoDoSistema() == null) {
-            throw new UnsupportedOperationException("O enum da ação do sistema" + pAcaoDoSistema + " não foi definido");
-        }
+            assertNotNull("O domínio da ação " + pAcaoDoSistema.getNomeUnico() + " não foi definido", pAcaoDoSistema.getEnumAcaoDoSistema().getDominio());
 
-        assertNotNull("O domínio da ação " + pAcaoDoSistema.getNomeUnico() + " não foi definido", pAcaoDoSistema.getEnumAcaoDoSistema().getDominio());
+            if (pAcaoDoSistema.isAcaoFormulario()) {
 
-        if (pAcaoDoSistema.isAcaoFormulario()) {
-
-            assertNotNull("O Xhtml da ação de formulario" + pAcaoDoSistema.getNomeUnico() + " está nula", ((ItfAcaoFormulario) pAcaoDoSistema).getXhtml());
-            assertNotEquals("O xhtml da acao  de formulario" + pAcaoDoSistema.getNomeUnico() + "está em branco", "", ((ItfAcaoFormulario) pAcaoDoSistema).getXhtml());
-            assertNotEquals("O xhtml da acao  de formulario" + pAcaoDoSistema.getNomeUnico() + "está em branco", A, ((ItfAcaoFormulario) pAcaoDoSistema).getXhtml());
-
-        }
-
-        if (pAcaoDoSistema.isTemAcaoPrincipal()) {
-            assertNotNull("A ação principal não foi definida em " + pAcaoDoSistema.getNomeUnico(), ((ItfAcaoSecundaria) pAcaoDoSistema).getAcaoPrincipal());
-        }
-
-        assertNotNull("O icone da ação " + pAcaoDoSistema.getIconeAcao() + " não foi definido", pAcaoDoSistema.getIconeAcao());
-
-        if (pAcaoDoSistema.isConfigurado()) {
-
-            // Verificando Configuração de XHTML
-            switch (pAcaoDoSistema.getTipoAcaoSistema()) {
-
-                case ACAO_ENTIDADE_FORMULARIO:
-                case ACAO_ENTIDADE_FORMULARIO_MODAL:
-                case ACAO_ENTIDADE_GERENCIAR:
-                    ItfAcaoFormulario acaoForm = (ItfAcaoFormulario) pAcaoDoSistema;
-                    if (acaoForm.getXhtml() == null || acaoForm.getXhtml() == "") {
-
-                        throw new UnsupportedOperationException("A ação de formulário " + acaoForm.getNomeUnico() + " não possui um xhtml cadastrado ");
-
-                    }
-
-                    break;
+                assertNotNull("O Xhtml da ação de formulario" + pAcaoDoSistema.getNomeUnico() + " está nula", ((ItfAcaoFormulario) pAcaoDoSistema).getXhtml());
+                assertNotEquals("O xhtml da acao  de formulario" + pAcaoDoSistema.getNomeUnico() + "está em branco", "", ((ItfAcaoFormulario) pAcaoDoSistema).getXhtml());
 
             }
 
-            // Verificando configuração de Metodo na camada Controller
-            switch (pAcaoDoSistema.getTipoAcaoSistema()) {
-
-                case ACAO_ENTIDADE_CONTROLLER:
-                case ACAO_CONTROLLER:
-                    ItfAcaoController acaocontroller = (ItfAcaoController) pAcaoDoSistema;
-
-                    if (acaocontroller.getIdMetodo() == 0) {
-
-                        throw new UnsupportedOperationException("O id do metodo da ação controller: " + pAcaoDoSistema.getNomeUnico()
-                                + "Não possui foi configurado"); //To change body of generated methods, choose Tools | Templates.
-
-                    }
-                    Method metodo = SBCore.getConfiguradorDePermissao().getMetodoByAcao(pAcaoDoSistema);
-                    if (metodo == null) {
-
-                        throw new UnsupportedOperationException("A ação de controller " + pAcaoDoSistema.getNomeUnico()
-                                + "Não possui nenhum metodo vinculado, nas classes de controller do sistema"); //To change body of generated methods, choose Tools | Templates.
-
-                    }
-                    break;
-
+            if (pAcaoDoSistema.isTemAcaoPrincipal()) {
+                assertNotNull("A ação principal não foi definida em " + pAcaoDoSistema.getNomeUnico(), ((ItfAcaoSecundaria) pAcaoDoSistema).getAcaoPrincipal());
             }
 
-            switch (pAcaoDoSistema.getTipoAcaoSistema()) {
-                case ACAO_DO_SISTEMA:
+            assertNotNull("O icone da ação " + pAcaoDoSistema.getIconeAcao() + " não foi definido", pAcaoDoSistema.getIconeAcao());
 
-                    break;
-                case ACAO_ENTIDADE_FORMULARIO:
-
-                case ACAO_ENTIDADE_FORMULARIO_MODAL:
-
-                case ACAO_ENTIDADE_CONTROLLER:
-
-                    ItfAcaoSecundaria acaoEntidadeSecundaria = (ItfAcaoSecundaria) pAcaoDoSistema;
-
-                    if (acaoEntidadeSecundaria.getAcaoPrincipal() == null) {
-                        throw new UnsupportedOperationException("esta ação é uma ação secondária, portando deve ser configurada uma ação Principal a ela.." + pAcaoDoSistema.getNomeUnico()); //To change body of generated methods, choose Tools | Templates.
-                    }
-
-                    break;
-
-                case ACAO_ENTIDADE_GERENCIAR:
-                    ItfAcaoEntidade acaoEntidade = (ItfAcaoEntidade) pAcaoDoSistema;
-
-                    break;
-
-                case ACAO_CONTROLLER:
-
-                    break;
-                case ACAO_SELECAO_DE_ACAO:
-
-                    break;
-                default:
-                    throw new AssertionError(pAcaoDoSistema.getTipoAcaoSistema().name());
-            }
             return true;
-        } else {
-            System.out.println("[SBINFO]: A AÇÃO " + pAcaoDoSistema.getNomeUnico() + " ainda não foi configurada");
-            if (validarAcoesNaoConfiguradas) {
-                return true;
-            } else {
-                return false;
-            }
+        } catch (Throwable t) {
+            lancarErroJUnit(t);
         }
+        return false;
     }
 
-}
-
-/**
- *
- * @param pValidarAcoesNaoCOnfiguradas True para validar todas as ações, False
- * para validar apenas aquelas que foram configuradas
- */
-public TesteAcaoDoSistema(boolean pValidarAcoesNaoCOnfiguradas) {
+    /**
+     *
+     * @param pValidarAcoesNaoCOnfiguradas True para validar todas as ações,
+     * False para validar apenas aquelas que foram configuradas
+     */
+    public TesteAcaoDoSistema(boolean pValidarAcoesNaoCOnfiguradas) {
         validarAcoesNaoConfiguradas = pValidarAcoesNaoCOnfiguradas;
     }
 
