98c829eaf661a7d0279b68197c3fc1ee49db3b93
Salvio Furbino <salviof@gmal.com>
2016-06-02 18:40:56 -0300

    Atualizacao automática

--- SB_FRAMEWORK/SBCore/src/main/java/com/super_bits/modulosSB/SBCore/InfoCampos/registro/ItemGenerico.java
+++ SB_FRAMEWORK/SBCore/src/main/java/com/super_bits/modulosSB/SBCore/InfoCampos/registro/ItemGenerico.java
@@ -411,7 +411,8 @@ public abstract class ItemGenerico extends Object implements ItfBeanGenerico, It
                 if (tipoDeValor.equals(String.class.toString())) {
                     valor = (String) pCampoReflexao.get(this);
                 } else // System.out.println("TTTTIIIPOOOO diferente de String:"+campoReflecao.getType().getName());
-                 if (pCampoReflexao.getType().getName().equals("int")) {
+                {
+                    if (pCampoReflexao.getType().getName().equals("int")) {
                         // System.out.println("TTTTIIIPOOOO int");
                         valor = (Integer) pCampoReflexao.get(this);
                     } else if (pCampoReflexao.getType().getName()
@@ -428,6 +429,7 @@ public abstract class ItemGenerico extends Object implements ItfBeanGenerico, It
                     } else {
                         return null;
                     }
+                }
                 return valor;
             } catch (IllegalArgumentException | IllegalAccessException e) {
                 FabErro.SOLICITAR_REPARO.paraDesenvolvedor("Erro Obtendo Valor do Campo tipo:" + pCampoReflexao, e);
@@ -624,6 +626,7 @@ public abstract class ItemGenerico extends Object implements ItfBeanGenerico, It
                         return null;
                     }
                 }
+                i++;
 
             }
 
