3abeebfa29e80bdc4260193d81e0e7be6c23b7c4
Salvio Furbino <salviof@gmal.com>
2016-07-16 02:11:59 -0300

    Atualizacao automática

--- SB_FRAMEWORK/SBAcessosModel/src/test/java/com/super_bits/modulos/SBAcessosModel/geradorCodigo/UtilSBGeradorDeCodigoTest.java
+++ SB_FRAMEWORK/SBAcessosModel/src/test/java/com/super_bits/modulos/SBAcessosModel/geradorCodigo/UtilSBGeradorDeCodigoTest.java
@@ -109,6 +109,7 @@ public class UtilSBGeradorDeCodigoTest extends TesteAcessosModelPadrao {
         try {
 
             EstruturaDeEntidade comprador = new EstruturaDeEntidade();
+
             EstruturaCampo campoID = new EstruturaCampo(FabCampos.ID.getRegistro());
             campoID.setNomeDeclarado("id");
             campoID.getMascara();
