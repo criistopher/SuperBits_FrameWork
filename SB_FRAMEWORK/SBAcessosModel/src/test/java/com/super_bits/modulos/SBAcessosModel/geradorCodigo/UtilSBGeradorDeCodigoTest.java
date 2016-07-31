b8cac67c909066f2af5c7fd066565ced4110df7c
Salvio Furbino <salviof@gmail.com>
2016-07-31 04:38:08 -0300

    Atualizacao automática

--- SB_FRAMEWORK/SBAcessosModel/src/test/java/com/super_bits/modulos/SBAcessosModel/geradorCodigo/UtilSBGeradorDeCodigoTest.java
+++ SB_FRAMEWORK/SBAcessosModel/src/test/java/com/super_bits/modulos/SBAcessosModel/geradorCodigo/UtilSBGeradorDeCodigoTest.java
@@ -9,24 +9,14 @@ import com.super_bits.Controller.Interfaces.ItfModuloAcaoSistema;
 import com.super_bits.Controller.Interfaces.acoes.ItfAcaoDoSistema;
 import com.super_bits.modulos.SBAcessosModel.TesteAcessosModelPadrao;
 import com.super_bits.modulos.SBAcessosModel.controller.FabAcaoSeguranca;
-import com.super_bits.modulos.SBAcessosModel.geradorCodigo.model.CalculoDeEntidade;
-import com.super_bits.modulos.SBAcessosModel.geradorCodigo.model.EstruturaCampo;
 import com.super_bits.modulos.SBAcessosModel.geradorCodigo.model.EstruturaDeEntidade;
-import com.super_bits.modulos.SBAcessosModel.geradorCodigo.model.LigacaoMuitosParaMuitos;
-import com.super_bits.modulos.SBAcessosModel.geradorCodigo.model.LigacaoMuitosParaUm;
-import com.super_bits.modulos.SBAcessosModel.geradorCodigo.model.LigacaoUmParaMuitos;
-import com.super_bits.modulos.SBAcessosModel.geradorCodigo.model.ListaDeEntidade;
-import com.super_bits.modulos.SBAcessosModel.model.acoes.AcaoDoSistema;
 import com.super_bits.modulosSB.SBCore.ConfigGeral.SBCore;
-import com.super_bits.modulosSB.SBCore.InfoCampos.campo.Campo;
-import com.super_bits.modulosSB.SBCore.InfoCampos.campo.FabCampos;
 import com.super_bits.modulosSB.SBCore.InfoCampos.registro.Interfaces.basico.FabTipoBeanSBGenerico;
-import com.super_bits.modulosSB.SBCore.ManipulaArquivo.UtilSBCoreArquivoTexto;
 import com.super_bits.modulosSB.SBCore.TratamentoDeErros.FabErro;
 import java.util.ArrayList;
 import java.util.List;
-import org.junit.Test;
 import org.junit.Before;
+import org.junit.Test;
 
 /**
  *
@@ -54,81 +44,81 @@ public class UtilSBGeradorDeCodigoTest extends TesteAcessosModelPadrao {
         ESTRUTURA_DE_ENTIDADE1.adicionarTags("Comprador,Cliente,Compra,Colaborador,Parceiro");
         ESTRUTURA_DE_ENTIDADE1.setTipoEntidade(FabTipoBeanSBGenerico.BEAN_CONTATO_CORPORATIVO);
         ESTRUTURA_DE_ENTIDADE1.adicionarEnum("GRUPO_FRM_LISTAR,USUARIO_FRM_EDITAR,GRUPO_FRM_NOVO,GRUPO_FRM_VISUALIZAR,GRUPO_CTR_ALTERAR_STATUS,GRUPO_MB_GERENCIAR");
-
-        EstruturaCampo campoID = new EstruturaCampo(FabCampos.ID.getRegistro(), ESTRUTURA_DE_ENTIDADE1);
-        campoID.setNomeDeclarado("id");
-        campoID.getMascara();
-        campoID.setLabel("Id");
-        campoID.setDescricao("Numero de registro");
-        ESTRUTURA_DE_ENTIDADE1.getCampos().add(campoID);
-
-        EstruturaCampo campoNome = new EstruturaCampo(FabCampos.AAA_DESCRITIVO.getRegistro(), ESTRUTURA_DE_ENTIDADE1);
-        campoNome.setNomeDeclarado("nome");
-        campoNome.getMascara();
-        campoNome.setLabel("nome descritivo");
-        campoNome.setDescricao("Descreve o nome da entidade");
-        ESTRUTURA_DE_ENTIDADE1.getCampos().add(campoNome);
-
-        EstruturaCampo campoRazao = new EstruturaCampo(FabCampos.TEXTO_SIMPLES.getRegistro(), ESTRUTURA_DE_ENTIDADE1);
-        campoRazao.setNomeDeclarado("razaoSocial");
-        campoRazao.getMascara();
-        campoRazao.setLabel("texto simples");
-        campoRazao.setDescricao("Um campo de texto simples");
-        ESTRUTURA_DE_ENTIDADE1.getCampos().add(campoRazao);
-
-        EstruturaCampo campoTelefoneNacional = new EstruturaCampo(FabCampos.TELEFONE_FIXO_NACIONAL.getRegistro(), ESTRUTURA_DE_ENTIDADE1);
-        campoTelefoneNacional.setNomeDeclarado("telefone");
-        campoTelefoneNacional.getMascara();
-        campoTelefoneNacional.setLabel("Telefone");
-        campoTelefoneNacional.setDescricao("Telefone fixo nacional");
-        ESTRUTURA_DE_ENTIDADE1.getCampos().add(campoTelefoneNacional);
-
-        EstruturaCampo campoCnpj = new EstruturaCampo(FabCampos.CNPJ.getRegistro(), ESTRUTURA_DE_ENTIDADE1);
-        campoCnpj.setNomeDeclarado("cnpj");
-        campoCnpj.getMascara();
-        campoCnpj.setLabel("CNPJ");
-        campoCnpj.setDescricao("NUmero de registro da empresa");
-        ESTRUTURA_DE_ENTIDADE1.getCampos().add(campoCnpj);
-
-        LigacaoUmParaMuitos campoFiliais = new LigacaoUmParaMuitos();
-        campoFiliais.setLabel("Filiais");
-        campoFiliais.setDescricao("Filiais do Comprador");
-        campoFiliais.setNomeDeclarado("filiais");
-        campoFiliais.setNomeEntidade("FilialComprador");
-        ESTRUTURA_DE_ENTIDADE1.getUmParaMuitos().add(campoFiliais);
-
-        LigacaoMuitosParaUm campoFilialPrincipal = new LigacaoMuitosParaUm();
-        campoFilialPrincipal.setLabel("Filial Principal");
-        campoFilialPrincipal.setDescricao("Filial Principal do Comprador");
-        campoFilialPrincipal.setNomeDeclarado("filialPrincipal");
-        campoFilialPrincipal.setNomeEntidade("FilialComprador");
-        ESTRUTURA_DE_ENTIDADE1.getMuitosParaUm().add(campoFilialPrincipal);
-
-        LigacaoMuitosParaMuitos campoPermitidos = new LigacaoMuitosParaMuitos();
-        campoPermitidos.setJoinTableName("Compradores_Permitidos");
-        campoPermitidos.setJoinColumName("acesso_id");
-        campoPermitidos.setInverseJoinColumName("comprador_id");
-        campoPermitidos.setNomeDeclarado("compradoresPermitidos");
-        campoPermitidos.setNomeEntidade("Comprador");
-
-        ESTRUTURA_DE_ENTIDADE1.getMuitosParaMuitos().add(campoPermitidos);
-
-        ListaDeEntidade campoLista = new ListaDeEntidade("ENUM_TESTE", "ENUM_TESTE", "Campanha", "Pedido", "");
-        campoLista.setNomeDeclaracao("declarado");
-        ESTRUTURA_DE_ENTIDADE1.getListas().add(campoLista);
-
-        CalculoDeEntidade campoCalculo = new CalculoDeEntidade("ENUM_TESTE", "ENUM_TESTE", "tipoRetorno", FabCampos.TELEFONE_FIXO_NACIONAL.getRegistro(), ESTRUTURA_DE_ENTIDADE1);
-        campoCalculo.setLabel("LABEL");
-        campoCalculo.setDescricao("DESCRIÇÃO TESTE");
-        campoCalculo.setNomeDeclarado("calculoLeroLero");
-        ESTRUTURA_DE_ENTIDADE1.getCalculos().add(campoCalculo);
-
-        //
-        //
-        // FINAL DA CRIAÇÃO DA ESTRUTURA DE ENTIDADE 1
-        //
-        //
-        //
+        /**
+         * EstruturaCampo campoID = new
+         * EstruturaCampo(FabCampos.ID.getRegistro(), ESTRUTURA_DE_ENTIDADE);
+         * campoID.setNomeDeclarado("id"); campoID.getMascara();
+         * campoID.setLabel("Id"); campoID.setDescricao("Numero de registro");
+         * ESTRUTURA_DE_ENTIDADE1.getCampos().add(campoID);
+         *
+         * EstruturaCampo campoNome = new
+         * EstruturaCampo(FabCampos.AAA_DESCRITIVO.getRegistro(),
+         * ESTRUTURA_DE_ENTIDADE1); campoNome.setNomeDeclarado("nome");
+         * campoNome.getMascara(); campoNome.setLabel("nome descritivo");
+         * campoNome.setDescricao("Descreve o nome da entidade");
+         * ESTRUTURA_DE_ENTIDADE1.getCampos().add(campoNome);
+         *
+         * EstruturaCampo campoRazao = new
+         * EstruturaCampo(FabCampos.TEXTO_SIMPLES.getRegistro(),
+         * ESTRUTURA_DE_ENTIDADE1); campoRazao.setNomeDeclarado("razaoSocial");
+         * campoRazao.getMascara(); campoRazao.setLabel("texto simples");
+         * campoRazao.setDescricao("Um campo de texto simples");
+         * ESTRUTURA_DE_ENTIDADE1.getCampos().add(campoRazao);
+         *
+         * EstruturaCampo campoTelefoneNacional = new
+         * EstruturaCampo(FabCampos.TELEFONE_FIXO_NACIONAL.getRegistro(),
+         * ESTRUTURA_DE_ENTIDADE1);
+         * campoTelefoneNacional.setNomeDeclarado("telefone");
+         * campoTelefoneNacional.getMascara();
+         * campoTelefoneNacional.setLabel("Telefone");
+         * campoTelefoneNacional.setDescricao("Telefone fixo nacional");
+         * ESTRUTURA_DE_ENTIDADE1.getCampos().add(campoTelefoneNacional);
+         *
+         * EstruturaCampo campoCnpj = new
+         * EstruturaCampo(FabCampos.CNPJ.getRegistro(), ESTRUTURA_DE_ENTIDADE1);
+         * campoCnpj.setNomeDeclarado("cnpj"); campoCnpj.getMascara();
+         * campoCnpj.setLabel("CNPJ"); campoCnpj.setDescricao("NUmero de
+         * registro da empresa");
+         * ESTRUTURA_DE_ENTIDADE1.getCampos().add(campoCnpj);
+         *
+         * LigacaoUmParaMuitos campoFiliais = new LigacaoUmParaMuitos();
+         * campoFiliais.setLabel("Filiais"); campoFiliais.setDescricao("Filiais
+         * do Comprador"); campoFiliais.setNomeDeclarado("filiais");
+         * campoFiliais.setNomeEntidade("FilialComprador");
+         * ESTRUTURA_DE_ENTIDADE1.getUmParaMuitos().add(campoFiliais);
+         *
+         * LigacaoMuitosParaUm campoFilialPrincipal = new LigacaoMuitosParaUm();
+         * campoFilialPrincipal.setLabel("Filial Principal");
+         * campoFilialPrincipal.setDescricao("Filial Principal do Comprador");
+         * campoFilialPrincipal.setNomeDeclarado("filialPrincipal");
+         * campoFilialPrincipal.setNomeEntidade("FilialComprador");
+         * ESTRUTURA_DE_ENTIDADE1.getMuitosParaUm().add(campoFilialPrincipal);
+         *
+         * LigacaoMuitosParaMuitos campoPermitidos = new
+         * LigacaoMuitosParaMuitos();
+         * campoPermitidos.setJoinTableName("Compradores_Permitidos");
+         * campoPermitidos.setJoinColumName("acesso_id");
+         * campoPermitidos.setInverseJoinColumName("comprador_id");
+         * campoPermitidos.setNomeDeclarado("compradoresPermitidos");
+         * campoPermitidos.setNomeEntidade("Comprador");
+         *
+         * ESTRUTURA_DE_ENTIDADE1.getMuitosParaMuitos().add(campoPermitidos);
+         *
+         * ListaDeEntidade campoLista = new ListaDeEntidade("ENUM_TESTE",
+         * "ENUM_TESTE", "Campanha", "Pedido", "");
+         * campoLista.setNomeDeclaracao("declarado");
+         * ESTRUTURA_DE_ENTIDADE1.getListas().add(campoLista);
+         *
+         * CalculoDeEntidade campoCalculo = new CalculoDeEntidade("ENUM_TESTE",
+         * "ENUM_TESTE", "tipoRetorno",
+         * FabCampos.TELEFONE_FIXO_NACIONAL.getRegistro(),
+         * ESTRUTURA_DE_ENTIDADE1); campoCalculo.setLabel("LABEL");
+         * campoCalculo.setDescricao("DESCRIÇÃO TESTE");
+         * campoCalculo.setNomeDeclarado("calculoLeroLero");
+         * ESTRUTURA_DE_ENTIDADE1.getCalculos().add(campoCalculo);
+         *
+         * // // // FINAL DA CRIAÇÃO DA ESTRUTURA DE ENTIDADE 1 // // //
+         */
     }
 
     @Before
