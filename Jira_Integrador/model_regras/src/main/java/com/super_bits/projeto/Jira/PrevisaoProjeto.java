a3af7923dafc1fd7658731973a66a20ccd6ef244
Salvio Furbino <salviof@gmail.com>
2016-08-22 22:10:23 -0300

    Atualizacao automática

---
+++ Jira_Integrador/model_regras/src/main/java/com/super_bits/projeto/Jira/PrevisaoProjeto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.super_bits.projeto.Jira;
+
+import java.util.List;
+
+/**
+ *
+ * @author desenvolvedor
+ */
+public class PrevisaoProjeto {
+
+    private FabTipoProfissional tipoProfissional;
+
+    private List<PrevisaoEntidade> entidadesPrevistas;
+    private List<PrevisaoGestaoEntidade> gestoesPrevista;
+
+}
