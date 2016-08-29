c0be326c85c00175020b7d0dd3e41acaf905643b
Salvio Furbino <salvioF@salvio.marketingparaweb.com.br>
2016-08-29 11:33:40 -0300

    Atualizacao automática

--- SuperBitsWPStarter/webApp/instalrSubModulosWebFrameWork.sh
+++ SuperBitsWPStarter/webApp/instalrSubModulosWebFrameWork.sh
@@ -11,8 +11,8 @@ if [ ! -d $DIRETORIO_WEBAPP_SERVLET ]; then
 fi
 cd ..
 cd ..
-rm $DIRETORIO_WEBAPP_SERVLET/WEB-INF -f -d -r
-rm $DIRETORIO_RESOURCES/SBComp -f -d -r
+rm $DIRETORIO_WEBAPP_SERVLET/WEB-INF -f -d 
+rm $DIRETORIO_RESOURCES/SBComp -f -d 
 git submodule add https://github.com/salviof/WEB-INF.git $DIRETORIO_WEBAPP_SERVLET/WEB-INF
 git submodule add https://github.com/salviof/SBComp.git $DIRETORIO_RESOURCES/SBComp
 #cd resources
--- SuperBitsWPStarter/webAppRequisitos/instalrSubModulosWebFrameWork.sh
+++ SuperBitsWPStarter/webAppRequisitos/instalrSubModulosWebFrameWork.sh
@@ -16,7 +16,7 @@ echo "_____________________________"
 echo "$DIRETORIO_RESOURCES"
 rm $DIRETORIO_WEBAPP_SERVLET/WEB-INF -f -d 
 rm $DIRETORIO_RESOURCES/SBComp -f -d 
-git submodule add https://github.com/salviof/WEB-INF.git $DIRETORIO_WEBAPP_SERVLET/WEB-INF
-git submodule add https://github.com/salviof/SBComp.git $DIRETORIO_RESOURCES/SBComp
+git submodule add https://github.com/salviof/WEB-INF.git $DIRETORIO_WEBAPP_SERVLET/WEB-INF --force
+git submodule add https://github.com/salviof/SBComp.git $DIRETORIO_RESOURCES/SBComp --force
 #cd resources
 
